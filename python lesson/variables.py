39f9894b758f812949efc8cf87f84d28e500b660
Sairam <adhautho@gmail.com>
2022-09-24 00:16:17 +0530

    added variables.py

---
+++ python lesson/variables.py
@@ -0,0 +1,97 @@
+title="the invincible"
+print(title)
+
+title="captain america - the first avenger"
+print(title)
+
+
+#this capitalizes names
+actor="chris evans"
+actor="chris pratt"
+print(actor.title())
+print(actor.upper())
+print(actor.lower())
+
+firstname="star"
+lastname="lord"
+fullname=f"{firstname} {lastname}"
+name = f"Hello, {fullname.title()}!"
+print(name)
+
+
+prifirstname="star"
+lastname="lord"
+fullname=f"{firstname} {lastname}"
+name = f"Hello, {fullname.title()}!"
+print ("\tStar Lord")
+
+
+name="guardians of the galaxy - volume I "
+print(name.title().rstrip())
+
+name="guardians of the galaxy - volume I "
+print(name.title())
+
+
+quote="The Cap said, ''I can do this all day.''"
+print(quote)
+
+
+
+quote="The Cap said, ''I can do this all day.''"
+famous_person=quote
+print(famous_person)
+
+
+print("\tThe Avengers")
+print("\nCaptain America \nIron Man \nThor Odinson \nHawkeye \nBlack Widow \nHulk")
+
+
+
+number=45_514_454_545
+print(number)
+
+cap, ironman, thor = 4, 7, 1
+print(6+2, 9-1, 2*4, 64/8)
+
+
+avengers= ['cap' , 'ironman' , 'thor' , 'hulk']
+print(avengers)
+
+avengers= ['cap' , 'ironman' , 'thor' , 'hulk']
+print(avengers[0])
+
+avengers= ['cap' , 'ironman' , 'thor' , 'hulk']
+print(avengers[3].title())
+
+
+
+avengers= ['cap' , 'ironman' , 'thor' , 'hulk']
+print(avengers[-2])
+
+
+avengers= ['cap' , 'ironman' , 'thor' , 'hulk']
+message=f" The first avenger was {avengers[0].title()}."
+print(message)
+
+
+avengers= ['cap' , 'ironman' , 'thor' , 'hulk']
+avengers[3]='Hawkeye'
+print(avengers)
+
+
+avengers= ['cap' , 'ironman' , 'thor' , 'hulk']
+avengers.append('hawkeye')
+print(avengers)
+
+
+avengers= ['cap' , 'ironman' , 'thor' , 'hulk']
+avengers.insert(0, 'hawkeye')
+print(avengers)
+
+
+avengers= ['cap' , 'ironman' , 'thor' , 'hulk']
+del avengers[3]
+print(avengers)
+
+
